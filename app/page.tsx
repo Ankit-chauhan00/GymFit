af29807c53937c903d210a89e6e074f14ee84d91
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-05-14 18:30:27 +0530

    fonts setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const page = () => {
-  return <div className="bg-amber-200">page</div>;
+  return (
+    <>
+      <div className="font-frans bg-amber-200 text-8xl">page</div>
+      <div className="font-iceland text-8xl">iceeland</div>
+      <div className="font-asap text-8xl">Asap</div>
+    </>
+  );
 };
 
 export default page;
